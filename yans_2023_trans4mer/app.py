a03ae241a2d0659bfc02fd4cb91cab96d0787174
Leon Kajikawa <0131reon254@gmail.com>
2023-08-29 16:35:02 +0900

    add comments

--- yans_2023_trans4mer/app.py
+++ yans_2023_trans4mer/app.py
@@ -11,13 +11,11 @@ openai.api_key = os.getenv("API_KEY")
 app_password = os.getenv("APP_PASSWORD")
 app_username = os.getenv("APP_USERNAME")
 
+
 def generate(prompt):
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="256x256"
-    )
-    return response['data'][0]['url']
+    response = openai.Image.create(prompt=prompt, n=1, size="256x256")
+    return response["data"][0]["url"]
+
 
 examples = [
     ["きのこの山"],
@@ -26,10 +24,15 @@ examples = [
 
 demo = gr.Interface(
     fn=generate,
-    inputs=gr.components.Textbox(lines=5, label="Prompt"),
+    # inputs=gr.components.Textbox(lines=5, label="Prompt"),
+    inputs=[
+        gr.components.Textbox(lines=5, label="Prompt"),
+        gr.Radio(["icon", "pictogram"]),
+    ],
     outputs=gr.components.Image(type="filepath", label="Generated Image"),
     flagging_options=[],
-    examples=examples
+    # examples=examples,
 )
 
 demo.launch(share=False, auth=(app_username, app_password))
+# https://www.gradio.app/docs/radio ラジオボタンの入力要素をリストで取得する
